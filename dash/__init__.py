c6b6c8c021c9ed8d22acf1dcb5d2530ae3178acc
Simon Chuang <simon.s.chuang@gmail.com>
2018-05-14 08:54:00 +0800

    add login required

--- dash/__init__.py
+++ dash/__init__.py
@@ -1,18 +1,36 @@
+from flask import Flask
+from flask_socketio import SocketIO
 import os
 
-from flask import Flask
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    # a default secret that should be overridden by instance config
+    SECRET_KEY='dev',
+    # store the database in the instance folder
+    DATABASE=os.path.join(app.instance_path, 'dash.sqlite'),
+)
 
+socketio = SocketIO(app, async_mode='gevent')
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-def create_app(test_config=None):
-    """Create and configure an instance of the Flask application."""
-    app = Flask(__name__, instance_relative_config=True)
+# register the database commands
+from dash import db
+db.init_app(app)
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-    # apply the blueprints to the app
-    from dash import hello
-    app.register_blueprint(hello.bp)
-    app.add_url_rule('/', endpoint='index')
+# apply the blueprints to the app
+#from cloud import auth, instance
+import dash.auth
+app.register_blueprint(dash.auth.bp)
+import dash.server
+app.register_blueprint(dash.server.bp)
 
-    return app
+app.add_url_rule('/', endpoint='index')
+
+app.config['SECRET_KEY'] = 'secret!'
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
--- dash/server.py
+++ dash/server.py
@@ -1,9 +1,13 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-bp = Blueprint('blog', __name__)
+from dash.auth import login_required
+from dash.db import get_db
+
+bp = Blueprint('server', __name__)
 
 
 @bp.route('/')
+@login_required
 def index():
     return render_template('index.html')
